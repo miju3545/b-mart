86f9cc13ca21b54e63b2195d04bbe63aa82cf904
miju3545 <miju3545@gmail.com>
2023-08-16 17:42:32 +0900

    feat: 전역 context 및 layout 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import {
+  UserContextProvider,
+  WishListContextProvider,
+  CartContextProvider,
+  OrderListContextProvider,
+} from "@/contexts";
+import { Layout } from "@/components/atom";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserContextProvider>
+        <WishListContextProvider>
+          <CartContextProvider>
+            <OrderListContextProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </OrderListContextProvider>
+          </CartContextProvider>
+        </WishListContextProvider>
+      </UserContextProvider>
+    </QueryClientProvider>
+  );
 }
